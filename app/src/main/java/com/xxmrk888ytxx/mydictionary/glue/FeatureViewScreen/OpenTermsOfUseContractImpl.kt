2621c5b474c21b3127758a5e683c4ccb37bd4aba
xXMRK888YTXx <aksehchikov@gmail.com>
2023-07-28 13:08:05 +0300

    [Feature] Linked OpenPrivacyPolicyUseCase with OpenPrivacyPolicyContract and OpenTermsOfUseUseCase with OpenTermsOfUseContract

--- app/src/main/java/com/xxmrk888ytxx/mydictionary/glue/FeatureViewScreen/OpenTermsOfUseContractImpl.kt
+++ app/src/main/java/com/xxmrk888ytxx/mydictionary/glue/FeatureViewScreen/OpenTermsOfUseContractImpl.kt
@@ -1,11 +1,14 @@
 package com.xxmrk888ytxx.mydictionary.glue.FeatureViewScreen
 
 import com.xxmrk888ytxx.featureviewscreen.contract.OpenTermsOfUseContract
+import com.xxmrk888ytxx.mydictionary.UseCase.OpenTermsOfUseUseCase.OpenTermsOfUseUseCase
 import javax.inject.Inject
 
-class OpenTermsOfUseContractImpl @Inject constructor() : OpenTermsOfUseContract {
+class OpenTermsOfUseContractImpl @Inject constructor(
+    private val termsOfUseUseCase: OpenTermsOfUseUseCase
+) : OpenTermsOfUseContract {
 
     override fun openTermsOfUse() {
-
+        termsOfUseUseCase.execute()
     }
 }
