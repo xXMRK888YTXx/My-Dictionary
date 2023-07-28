dee2f4859e4771db48c9d943842ab1b2f3a9a429
xXMRK888YTXx <aksehchikov@gmail.com>
2023-07-28 13:03:27 +0300

    [Feature] Written OpenPrivacyPolicyUseCase

---
+++ app/src/main/java/com/xxmrk888ytxx/mydictionary/UseCase/OpenPrivacyPolicyUseCase/OpenPrivacyPolicyUseCaseImpl.kt
@@ -0,0 +1,15 @@
+package com.xxmrk888ytxx.mydictionary.UseCase.OpenPrivacyPolicyUseCase
+
+import android.content.Context
+import javax.inject.Inject
+
+class OpenPrivacyPolicyUseCaseImpl @Inject constructor(
+    private val context: Context
+) : OpenPrivacyPolicyUseCase {
+
+    override fun execute() {
+        try {
+
+        }catch (_:Exception) {}
+    }
+}
