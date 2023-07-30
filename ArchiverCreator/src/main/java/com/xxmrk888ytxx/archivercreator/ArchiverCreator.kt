326db375deaea7e6351a8d8078ea8c78426f312c
xXMRK888YTXx <aksehchikov@gmail.com>
2023-07-30 15:20:01 +0300

    [Docs] Documented module ArchiverCreator

--- ArchiverCreator/src/main/java/com/xxmrk888ytxx/archivercreator/ArchiverCreator.kt
+++ ArchiverCreator/src/main/java/com/xxmrk888ytxx/archivercreator/ArchiverCreator.kt
@@ -2,6 +2,17 @@ package com.xxmrk888ytxx.archivercreator
 
 import java.io.File
 
+
+/**
+ * [Ru]
+ *
+ * Интерфейс для создания архивов
+ */
+
+/**
+ * [En]
+ * Interface for creating archivers
+ */
 interface ArchiverCreator {
 
     fun createArchiver(file: File) : ArchiverFile
--- ArchiverCreator/src/main/java/com/xxmrk888ytxx/archivercreator/ArchiverFile.kt
+++ ArchiverCreator/src/main/java/com/xxmrk888ytxx/archivercreator/ArchiverFile.kt
@@ -4,14 +4,51 @@ import net.lingala.zip4j.ZipFile
 import java.io.Closeable
 import java.io.File
 
+/**
+ * [Ru]
+ *  Абстракция для управления файлом архива
+ */
+
+/**
+ * [En]
+ * Abstraction for archive file management
+ */
 abstract class ArchiverFile(
     val fileLocation:File,
     protected val zipFile: ZipFile
 ) : Closeable {
 
+    /**
+     * [Ru]
+     *  Метод для добавления файла в архив
+     */
+
+    /**
+     * [En]
+     * Method for adding file to archiver
+     */
     abstract suspend fun addFile(file: File)
 
+    /**
+     * [Ru]
+     * Распаковывает файлы из архива в переданную директорию
+     */
+
+    /**
+     * [En]
+     * Extract files from archiver to the transferred directory
+     */
     abstract suspend fun extractTo(file: File)
+
+    /**
+     * [Ru]
+     *  Возвращает названия файлов в архиве
+     */
+
+    /**
+     * [En]
+     * Returns files in the archiver
+     */
 
     abstract val innerFilesName:List<String>
 
