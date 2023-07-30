faa1940d8a395ff47c28ee117491b76b16171ece
xXMRK888YTXx <aksehchikov@gmail.com>
2023-07-30 10:51:36 +0300

    [Feature] Implemented OpenPrivacyPolicyUseCase and OpenTermsOfUseUseCase

--- app/src/main/java/com/xxmrk888ytxx/mydictionary/UseCase/OpenPrivacyPolicyUseCase/OpenPrivacyPolicyUseCaseImpl.kt
+++ app/src/main/java/com/xxmrk888ytxx/mydictionary/UseCase/OpenPrivacyPolicyUseCase/OpenPrivacyPolicyUseCaseImpl.kt
@@ -1,15 +1,33 @@
 package com.xxmrk888ytxx.mydictionary.UseCase.OpenPrivacyPolicyUseCase
 
 import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.net.Uri
+import com.xxmrk888ytxx.coreandroid.ShareInterfaces.Logger
+import com.xxmrk888ytxx.mydictionary.R
 import javax.inject.Inject
 
 class OpenPrivacyPolicyUseCaseImpl @Inject constructor(
-    private val context: Context
+    private val context: Context,
+    private val logger: Logger
 ) : OpenPrivacyPolicyUseCase {
 
     override fun execute() {
         try {
+            val url = context.getString(R.string.privacy_policy_url)
 
-        }catch (_:Exception) {}
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+                flags = FLAG_ACTIVITY_NEW_TASK
+            }
+
+            context.startActivity(browserIntent)
+        }catch (e:Exception) {
+            logger.error(e, LOG_TAG)
+        }
+    }
+    
+    companion object {
+        private const val LOG_TAG = "OpenPrivacyPolicyUseCaseImpl"
     }
 }
