e7b025f5d1014405dcd7b86fc45883cf5ea30f30
xXMRK888YTXx <aksehchikov@gmail.com>
2023-07-31 14:35:31 +0300

    [Docs] Updated documentation in  BackupConverter module

--- BackupConverter/src/main/java/com/xxmrk888ytxx/backupconverter/BackupImportConverter.kt
+++ BackupConverter/src/main/java/com/xxmrk888ytxx/backupconverter/BackupImportConverter.kt
@@ -5,11 +5,50 @@ import com.xxmrk888ytxx.backupconverter.models.LanguagesBackupModel
 import com.xxmrk888ytxx.backupconverter.models.WordGroupBackupModel
 import com.xxmrk888ytxx.coreandroid.ShareInterfaces.Logger
 
+/**
+ * [Ru]
+ * Интерфейс для конвертации данных бекапа в формате JSON в модели данных приложения
+ */
+
+/**
+ * [En]
+ * Interface for convert data of backup in JSON format to data models of application
+ */
 interface BackupImportConverter {
+
+    /**
+     * [Ru]
+     * Конвертация хедера в формате JSON в модель для приложения
+     */
+
+    /**
+     * [En]
+     * Convert header in JSON format to model for the application
+     */
 
     suspend fun jsonHeaderStringToModel(jsonHeaderString: String) : BackupHeader
 
+    /**
+     * [Ru]
+     * Конвертация списка языков в формате JSON в модель для приложения
+     */
+
+    /**
+     * [En]
+     * Convert list of languages in JSON format to model for the application
+     */
+
     suspend fun jsonLanguageToLanguageList(jsonString:String) : List<LanguagesBackupModel>
+
+    /**
+     * [Ru]
+     * Конвертация группы слов в формате JSON в модель для приложения
+     */
+
+    /**
+     * [En]
+     * Convert group of words in JSON format to model for the application
+     */
 
     suspend fun jsonWordGroupToWordGroup(jsonString: String) : WordGroupBackupModel
 
