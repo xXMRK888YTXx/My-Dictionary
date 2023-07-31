9e9ccb609e675016bd7696dde368b8782e1ba37d
xXMRK888YTXx <aksehchikov@gmail.com>
2023-07-31 11:18:14 +0300

    [Feature] Up version code to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.xxmrk888ytxx.mydictionary"
         minSdk = Config.minSdk
         targetSdk = Config.compileSdk
-        versionCode = 1
+        versionCode = 2
         versionName = Config.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
