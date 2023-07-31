12c88fca7663dd032f9c5ec5ff1528b855e5e8f3
xXMRK888YTXx <aksehchikov@gmail.com>
2023-07-31 16:10:22 +0300

    [Docs] Updated docs for LanguageDeterminant module

--- LanguageDeterminant/src/main/java/com/xxmrk888ytxx/languageindificator/LanguageDeterminant.kt
+++ LanguageDeterminant/src/main/java/com/xxmrk888ytxx/languageindificator/LanguageDeterminant.kt
@@ -3,8 +3,28 @@ package com.xxmrk888ytxx.languageindificator
 import kotlinx.coroutines.CoroutineScope
 import kotlin.jvm.Throws
 
+/**
+ * [Ru]
+ * Интерфейс для определения языка строки
+ */
+
+/**
+ * [En]
+ * Interface for determining the string language
+ */
 interface LanguageDeterminant {
 
+    /**
+     * [Ru]
+     * Возвращает код языка строки,
+     * если определить язык невозможно, то выбрасывает исключение [LanguageNotIdentifiedException]
+     */
+
+    /**
+     * [En]
+     * Return language code of string,
+     * if determinate language is impossible, it will be throw exception [LanguageNotIdentifiedException]
+     */
     @Throws(LanguageNotIdentifiedException::class)
     suspend fun getLanguageCodeFromText(text:String) : String
 
