4942df2eee0402c2f064c3a95fd4430c1b941ddd
xXMRK888YTXx <aksehchikov@gmail.com>
2023-08-03 15:13:15 +0300

    [Feature] Created release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.xxmrk888ytxx.mydictionary"
         minSdk = Config.minSdk
         targetSdk = Config.compileSdk
-        versionCode = 3
+        versionCode = 4
         versionName = Config.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
