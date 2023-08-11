5b9937a861ac093823b74425ccd6d49d2df69e83
xXMRK888YTXx <aksehchikov@gmail.com>
2023-08-11 10:08:14 +0300

    [Feature] Created method for upload file in TelegramApi

--- TelegramApi/src/main/java/com/xxmrk888ytxx/telegramapi/TelegramApi.kt
+++ TelegramApi/src/main/java/com/xxmrk888ytxx/telegramapi/TelegramApi.kt
@@ -3,10 +3,14 @@ package com.xxmrk888ytxx.telegramapi
 import com.xxmrk888ytxx.telegramapi.exception.ApiException
 import com.xxmrk888ytxx.telegramapi.exception.NoConnectionException
 import com.xxmrk888ytxx.telegramapi.exception.UnknownException
+import java.io.File
 import kotlin.jvm.Throws
 
 interface TelegramApi {
 
     @Throws(NoConnectionException::class,ApiException::class,UnknownException::class)
     suspend fun sendMessage(text:String) : Result<Unit>
+
+    @Throws(NoConnectionException::class,ApiException::class,UnknownException::class)
+    suspend fun uploadFile(fileBytes: ByteArray,caption:String? = null) : Result<Unit>
 }
--- TelegramApi/src/main/java/com/xxmrk888ytxx/telegramapi/TelegramKtorApi.kt
+++ TelegramApi/src/main/java/com/xxmrk888ytxx/telegramapi/TelegramKtorApi.kt
@@ -6,13 +6,17 @@ import com.xxmrk888ytxx.telegramapi.exception.UnknownException
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.request.forms.formData
+import io.ktor.client.request.forms.submitFormWithBinaryData
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.statement.bodyAsText
 import io.ktor.client.statement.request
+import io.ktor.http.HttpMethod
 import io.ktor.utils.io.errors.IOException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 import java.net.ConnectException
 import java.net.UnknownHostException
 
@@ -25,6 +29,9 @@ internal class TelegramKtorApi(
     private val sendMessageUrl:String
         get() = "https://api.telegram.org/bot$botKey/sendMessage"
 
+    private val sendDocumentUrl:String
+        get() = "https://api.telegram.org/bot$botKey/sendDocument"
+
     override suspend fun sendMessage(text: String): Result<Unit> = withContext(Dispatchers.IO) {
         return@withContext try {
 
@@ -32,6 +39,46 @@ internal class TelegramKtorApi(
                 parameter("chat_id",userId)
                 parameter("text",text)
             }
+
+            when(result.status.value) {
+                in 200..299 -> Result.success(Unit)
+                in 400..499 -> Result.failure(ApiException())
+                else -> Result.failure(UnknownException())
+            }
+
+        }
+        catch (e:ClientRequestException) {
+            return@withContext Result.failure(ApiException(e.stackTraceToString()))
+        }
+        catch (e: IOException) {
+            return@withContext Result.failure(NoConnectionException(e.stackTraceToString()))
+        }
+        catch (e:Exception) {
+            Result.failure(UnknownException(e.stackTraceToString()))
+        }
+    }
+
+    override suspend fun uploadFile(fileBytes: ByteArray,caption:String?): Result<Unit> = withContext(Dispatchers.IO) {
+        return@withContext try {
+
+            val formData = formData {
+                append("document",fileBytes)
+            }
+
+            val result = httpClient.submitFormWithBinaryData(
+                url = sendDocumentUrl,
+                formData = formData,
+            ) {
+                parameter("chat_id",userId)
+
+                if(caption != null) {
+                    parameter("caption",caption)
+                }
+
+                method = HttpMethod.Post
+            }
+
+
 
             when(result.status.value) {
                 in 200..299 -> Result.success(Unit)
