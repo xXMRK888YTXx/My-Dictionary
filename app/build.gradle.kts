24d7ad7240f720f6839db6ae1a4b80bb0c4c7ced
xXMRK888YTXx <aksehchikov@gmail.com>
2023-10-01 17:40:02 +0300

    [Chore] Up app version to 1.2.0(version code 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.xxmrk888ytxx.mydictionary"
         minSdk = Config.minSdk
         targetSdk = Config.compileSdk
-        versionCode = 8
+        versionCode = 9
         versionName = Config.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
