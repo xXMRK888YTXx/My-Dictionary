21f61b9f8adb5e8202d8076dbdbe9ee0857a816c
xXMRK888YTXx <aksehchikov@gmail.com>
2023-10-01 15:12:16 +0300

    [Feature] Implemented ProvideTranslateModelsContract

--- ManageModelsForTranslateScreen/src/main/java/com/xxmrk888ytxx/managetranslatedmodelsscreen/models/TranslateModel.kt
+++ ManageModelsForTranslateScreen/src/main/java/com/xxmrk888ytxx/managetranslatedmodelsscreen/models/TranslateModel.kt
@@ -3,6 +3,6 @@ package com.xxmrk888ytxx.managetranslatedmodelsscreen.models
 import androidx.annotation.IdRes
 
 data class TranslateModel(
-    @IdRes val name:Int,
+    @IdRes val name:Int?,
     val code:String
 )
--- app/src/main/java/com/xxmrk888ytxx/mydictionary/glue/ManageModelsForTranslateScreen/ProvideTranslateModelsContractImpl.kt
+++ app/src/main/java/com/xxmrk888ytxx/mydictionary/glue/ManageModelsForTranslateScreen/ProvideTranslateModelsContractImpl.kt
@@ -2,15 +2,25 @@ package com.xxmrk888ytxx.mydictionary.glue.ManageModelsForTranslateScreen
 
 import com.xxmrk888ytxx.managetranslatedmodelsscreen.contracts.ProvideTranslateModelsContract
 import com.xxmrk888ytxx.managetranslatedmodelsscreen.models.TranslateModel
+import com.xxmrk888ytxx.mydictionary.domain.SupportedLanguagesForTranslateProvider.SupportedLanguagesForTranslateProvider
+import com.xxmrk888ytxx.translator.Translator
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ProvideTranslateModelsContractImpl @Inject constructor(
-
+    private val translator: Translator,
+    private val supportedLanguagesForTranslateProvider: SupportedLanguagesForTranslateProvider
 ) : ProvideTranslateModelsContract {
 
-    override val models: Flow<ImmutableList<TranslateModel>> = flowOf(persistentListOf())
+    override val models: Flow<ImmutableList<TranslateModel>> = translator.downloadedModel.map { list ->
+        list.map { TranslateModel(
+            name = supportedLanguagesForTranslateProvider.supportedLanguageForTranslateMap[it.code]?.name,
+            code = it.code
+        ) }.toImmutableList()
+    }
 }
